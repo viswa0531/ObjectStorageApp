0083f5f38bdc6defcb72846ddac6f2bbe37f189a
viswa0531 <42704442+viswa0531@users.noreply.github.com>
2019-03-31 15:56:08 -0700

    Modified port number from 8080 to 5001

--- application.py
+++ application.py
@@ -199,7 +199,7 @@ if __name__ == "__main__":
     gunicorn_logger = logging.getLogger('gunicorn.error')
     application.logger.handlers = gunicorn_logger.handlers
     application.logger.setLevel(gunicorn_logger.level)
-    application.run(host='0.0.0.0', port=8080, debug=True)
+    application.run(host='0.0.0.0', port=5001, debug=True)
     #application.debug=True
     #application.run()
     #application.run(debug=True)
